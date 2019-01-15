4ffd06b18a697ff279e23333ca927fa4aef2686e
Christian Schläppi <christian.schlaeppi@codedump.ch>
2019-01-15 17:27:45 +0100

    Simplified check for presence of power adapter

--- battery/battery.py
+++ battery/battery.py
@@ -43,8 +43,11 @@ def main():
 		print(output_map['b_unknown'][1])
 		sys.exit(0)
 
+	secsleft = battery.secsleft  # by default
+
 	if battery.power_plugged:
 		output = output_map['b_plugged']
+		secsleft = 0  # irrelevant at this point.
 	elif 100 >= battery.percent >= 60:
 		output = output_map['b_high']
 	elif 59 >= battery.percent >= 16:
@@ -52,14 +55,10 @@ def main():
 	elif 15 >= battery.percent >= 0:
 		output = output_map['b_crit']
 
-	if not isinstance(battery.secsleft, int):
-		secsleft = 0
-	else:
-		secsleft = battery.secsleft
-
 	timeleft = datetime.timedelta(seconds=secsleft)
+
 	# icon - percentage - livetime
-	fmt = '{0} - {1:.1f}% - {2}\n'
+	fmt = '{0}: - {1:.1f}% - {2}\n'
 	out = fmt.format(
 		output[0],
 		battery.percent,
